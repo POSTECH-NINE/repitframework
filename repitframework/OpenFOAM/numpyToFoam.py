fixed norm-denorm; precision updated

repitframework/OpenFOAM/numpyToFoam.py
@@ -5,7 +5,7 @@ import re
 import numpy as np
 
 from repitframework.config import OpenfoamConfig
-from .utils import OpenfoamUtils
+from repitframework.OpenFOAM.utils import OpenfoamUtils
 
 def parse_numpy(data: np.ndarray) -> str:
 	"""
@@ -34,9 +34,7 @@ def parse_numpy(data: np.ndarray) -> str:
 		elif data.shape[-1] == 2: # 2D array
 			# For the vector field, OpenFOAM requires the data to be in the form of (x y z) for each row.
 			# So, if we have a 2D array of shape (n, 2), we need to add a column of zeros to make it (n, 3).
-			zero_columns = np.zeros((data.shape[0], 1))
-			data = np.concatenate((data, zero_columns), axis=1)
-			lines = ['(' + ' '.join(map(str, row)) + ')' for row in data]
+			lines = ['(' + ' '.join(map(str, row)) + ' 0)' for row in data]
 			return '\n'.join(lines)
 		else: # 3D array
 			lines = ['(' + ' '.join(map(str, row)) + ')' for row in data]
@@ -44,6 +42,79 @@ def parse_numpy(data: np.ndarray) -> str:
 	else:
 		raise ValueError("Data shape not supported. Aborting conversion from numpy to OpenFOAM.")
 
+def manage_time_uniform(solver_dir:Path, latestML_time:float) -> str:
+	'''
+	Changing time folder
+	---------------------
+	command::
+
+		foamDictionary -case solver_dir -entry value -set latestML_time latestCFD_time/uniform/time
+
+		foamDictionary -case solver_dir -entry name -set '"latestML_time"' latestCFD_time/0/time
+
+		foamDictionary -case solver_dir -entry index -set latestML_time_without_decimal latestCFD_time/constant/time
+
+	It also replaces the location values in every files inside the time directory. 
+	Files like U, p, T, uniform/time, etc.
+	'''
+	# Because in the directory, other files are also present which still have the old time values.
+	files_list = []
+	time_dir = Path.joinpath(solver_dir, f"{latestML_time}")
+	for file in time_dir.iterdir():
+		if file.is_file():
+			files_list.append(file)
+
+	uniform_time_dir = Path.joinpath(solver_dir, f"{latestML_time}/uniform/time")
+	if uniform_time_dir.exists(): files_list.append(uniform_time_dir)
+
+	for file in files_list:
+		if file == uniform_time_dir:
+			replace_string = "/uniform"
+		else:
+			replace_string = ""
+
+		with open(file, "r") as f:
+			data = f.read()
+			foam_data = re.sub(r'(location\s*)"([^"]*)"',rf'\1"{latestML_time}{replace_string}"',data)
+		with open(file, "w") as f:
+			f.write(foam_data)
+
+	command_to_change_time_value = ["foamDictionary", 
+								 "-case", 
+								 solver_dir, 
+								 "-entry", 
+								 "value", 
+								 "-set", 
+								 f"{latestML_time}", 
+								 f"{latestML_time}/uniform/time"]
+	
+	command_to_change_time_name = ["foamDictionary",
+								 "-case",
+								 solver_dir,
+								 "-entry",
+								 "name",
+								 "-set",
+								 f'"{latestML_time}"',
+								 f"{latestML_time}/uniform/time"]
+	
+	latestML_time_without_decimal = int(str(latestML_time).replace(".",""))
+	if latestML_time.is_integer(): latestML_time_without_decimal = int(f"{latestML_time}00")
+	command_to_change_time_index = ["foamDictionary",
+								 "-case",
+								 solver_dir,
+								 "-entry",
+								 "index",
+								 "-set",
+								 f"{latestML_time_without_decimal}",
+								 f"{latestML_time}/uniform/time"]
+	
+	output_value = subprocess.run(command_to_change_time_value, check=True, capture_output=True, text=True)
+	output_name = subprocess.run(command_to_change_time_name, check=True, capture_output=True, text=True)
+	output_index = subprocess.run(command_to_change_time_index, check=True, capture_output=True, text=True)
+
+	output_string = f"{output_value.stdout}\n{output_name.stdout}\n{output_index.stdout}"
+
+	return output_string
 
 def numpyToFoam(openfoam_config:OpenfoamConfig, 
 				latestML_time:float,
@@ -51,7 +122,7 @@ def numpyToFoam(openfoam_config:OpenfoamConfig,
 				variables:list=None,
 				solver_dir:Path=None,
 				assets_path:Path=None,
-				is_ground_truth:bool=False) -> bool:
+				is_ground_truth:bool=False) -> str:
 	"""
 	This function takes a numpy file and writes it to an OpenFOAM file.
 
@@ -103,6 +174,7 @@ def numpyToFoam(openfoam_config:OpenfoamConfig,
 
 	latestCFD_time_dir = Path.joinpath(solver_dir,f"{latestCFD_time}") # time directory for current time
 
+
 	# Because, in openfoam if the time directory is float at the terminal values like; 11.0, 12.0, 13.0 are converted to 11, 12, 13.
 	ml_dir_time_name = int(latestML_time) if latestML_time.is_integer() else latestML_time
 	latestML_time_dir  = Path.joinpath(solver_dir, f"{ml_dir_time_name}") # time directory for next time (latestCFD_time + 1)
@@ -110,14 +182,23 @@ def numpyToFoam(openfoam_config:OpenfoamConfig,
 	# copy the contents of latest CFD simulation time to the latest ML simulation time.
 	if not latestML_time_dir.exists(): subprocess.run(["cp", "-r", latestCFD_time_dir, latestML_time_dir], check=True)
 
+	output_string = manage_time_uniform(solver_dir, ml_dir_time_name)
+
 	for variable in variables:
 		numpy_file_name = f"{variable}_{latestML_time}.npy" if is_ground_truth else f"{variable}_{latestML_time}_predicted.npy"
 		openfoam_var_path = Path.joinpath(latestML_time_dir, f"{variable}") # openfoam variable path: where we write the numpy data    
 		numpy_file_path =   Path.joinpath(assets_path, numpy_file_name) # numpy file path: where the numpy data is stored
+
 		# numpy file processing:
 		data = np.load(numpy_file_path)
-		data_str = "(\n" + parse_numpy(data) + "\n)\n;" # convert numpy data to OpenFOAM format
 
+		if variable == "T":
+			data = np.round(data, 9)
+		else:
+			data = np.round(data, 18)
+
+		data_str = "(\n" + parse_numpy(data) + "\n)\n;" # convert numpy data to OpenFOAM format
+		
 		with open(openfoam_var_path, "r") as file:
 			foam_data_temp = file.read()
 			foam_data = re.sub(r'(location\s*)"([^"]*)"',rf'\1"{latestML_time}"',foam_data_temp) # update the location to the next time step
@@ -125,9 +206,10 @@ def numpyToFoam(openfoam_config:OpenfoamConfig,
 
 		with open(openfoam_var_path, "w") as file:
 			file.write(foam_data)
-	return True    
+	return output_string   
 
 	
 if __name__ == "__main__":
 	openfoam_config = OpenfoamConfig()
-	numpyToFoam(openfoam_config, latestCFD_time=10.0, latestML_time=20.0, is_ground_truth=True)
+	output_string = numpyToFoam(openfoam_config, latestCFD_time=10.0, latestML_time=10.51, is_ground_truth=True)
+	print(output_string)

repitframework/OpenFOAM/utils.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import subprocess
 from datetime import datetime
+from typing import List
 
 import Ofpp
 import numpy as np
@@ -85,6 +86,16 @@ class OpenfoamUtils:
         return solver_type.strip()
     
     @staticmethod
+    def generate_intervals(start_time:int|float, end_time:int|float, time_step:int|float, round_to:int=2) -> list:
+        time_list = []
+        time_list.append(round(start_time, round_to))
+        running_time = start_time
+        while running_time <= end_time:
+            running_time += time_step # To include end_time also in the list. It is kept before append.
+            time_list.append(round(running_time, round_to))
+        return time_list
+
+    @staticmethod
     def run_subprocess(command_list:list):
         '''
         Run the subprocess command and return the output.
@@ -94,10 +105,12 @@ class OpenfoamUtils:
 
     @staticmethod
     def parse_to_numpy(openfoam_config:OpenfoamConfig,
+                       start_time:float,
+                       end_time:float,
                        solver_dir:Path=None,
                        save_path:Path=None,
                        variables:list=None,
-                       time_list:list[str]=None,
+                       write_interval:float=0.01,
                        del_dirs:bool=False) -> Path:
         '''
         OpenFOAM stores the data in the form of Dictionary(OpenFOAM type) files. But to train the model
@@ -141,19 +154,24 @@ class OpenfoamUtils:
         solver_dir = solver_dir if solver_dir else openfoam_config.solver_dir
         save_path = save_path if save_path else openfoam_config.assets_path
         variables = variables if variables else openfoam_config.extend_variables()
+        write_interval = write_interval if write_interval else openfoam_config.write_interval
+        round_to = openfoam_config.round_to
+        time_list: List[int|float] = OpenfoamUtils.generate_intervals(start_time, end_time, write_interval, round_to)
 
-        if not time_list:# List the time directories
-            commands_to_list_time_directories = ["foamListTimes", "-case", solver_dir]
-            time_list = OpenfoamUtils.run_subprocess(commands_to_list_time_directories).split("\n")
-            time_list = [i for i in time_list if i]
-        time_directories = [Path(solver_dir,i) for i in time_list]
+        time_list = [int(time) if time.is_integer() else time for time in time_list]
+        time_directories = [Path(solver_dir,str(i)) for i in time_list]
 
         # Parse the data to numpy
         for time_dir in time_directories:
             for var in variables:
                 try:
                     data = Ofpp.parse_internal_field(Path(time_dir, var))
-                    openfoam_config.logger.debug(f"Data parsed to numpy:{var}_{time_dir.name} --> {data.shape}")
+                    if var == "T":
+                        data = np.round(data, 9)
+                    else:
+                        data = np.round(data, 18)
+                        
+                    openfoam_config.logger.debug(f"Data parsed to numpy:{var}_{float(time_dir.name)} --> {data.shape}")
                     np.save(Path(save_path, f"{var}_{float(time_dir.name)}.npy"), data) # We are saving it in float because we want to keep things consistent.
                 except Exception as e:
                     openfoam_config.logger.exception(f"Error in parsing the data to numpy: {e}")
@@ -252,7 +270,7 @@ class OpenfoamUtils:
     def run_solver(self, start_time:int|float=None,
                    end_time:int|float=None,
                    write_interval:int|float=None,
-                   save_to_numpy:bool=False,
+                   save_to_numpy:bool=True,
                    del_dirs:bool=False) -> bool:
         '''
         This method aims at running the CFD solver of your interest. 
@@ -338,11 +356,16 @@ class OpenfoamUtils:
         self.openfoam_config.logger.debug(f"\n Solver Output: {solver_result}\n")
 
         if save_to_numpy:
-            self.parse_to_numpy(self.openfoam_config, del_dirs=del_dirs)
+            self.parse_to_numpy(openfoam_config=self.openfoam_config, 
+                                start_time=start_time, 
+                                end_time=end_time,
+                                write_interval=write_interval,
+                                del_dirs=del_dirs)
 
         return True
 
 if __name__ == "__main__":
     openfoam_config = OpenfoamConfig()
     openfoam_utils = OpenfoamUtils(openfoam_config)
-    openfoam_utils.run_solver(start_time=10, end_time=20, write_interval=0.01,save_to_numpy=False, del_dirs=False)
+    openfoam_utils.run_solver(start_time=10.51, end_time=20.0, write_interval=0.01,save_to_numpy=True, del_dirs=False)
+    # openfoam_utils.parse_to_numpy(openfoam_config, start_time=10.51, end_time=20.0)

repitframework/config.py
@@ -163,7 +163,7 @@ class TrainingConfig(BaseConfig):
 		self.training_end_time = 10.02
 		self.prediction_start_time = 10.02
 		self.prediction_end_time = 20.0
-		self.bc_type:str = None # either None or "ground_truth"
+		self.bc_type:str = "ground_truth" # either None or "ground_truth"
 
 		self.log_file: Path = Path("Training.log")
 		self.logger = self.setup_logger("TrainingLogger",self.log_file)
@@ -216,14 +216,30 @@ class TrainingConfig(BaseConfig):
 		uy_matrix = np.pad(uy_matrix, ((1,1),(1,1)), mode="constant", constant_values=0)
 		t_matrix = np.pad(t_matrix, ((1,1),(1,1)), mode="constant", constant_values=0)
 
-		right_wall_temperature = 307.75
-		left_wall_temperature = 288.15
+		'''
+		Applying the boundary conditions:
+		While reshaping the data, we are using order="F" to try to keep the data in the same order as OpenFOAM.
+		But the numpy puts lower wall at the top and upper wall at the bottom. Hence, we need to flip.
+		Naturally: 
+			- Top wall is the cold wall
+			- Bottom wall is the hot wall
 
-		# Applying the boundary conditions
-		t_matrix[:, 0] = left_wall_temperature
-		t_matrix[:, -1] = right_wall_temperature
-		t_matrix[0, :] = t_matrix[1,:]
-		t_matrix[-1, :] = t_matrix[-2,:]
+		Numpy:
+			- Top wall is the hot wall
+			- Bottom wall is the cold wall
+
+		But if order="C":
+			- Left wall is the hot wall
+			- Right wall is the cold wall
+		'''
+
+		top_wall_temperature = 307.75
+		bottom_wall_temperature = 288.15
+
+		t_matrix[:, 0] = t_matrix[:, 1]
+		t_matrix[:, -1] = t_matrix[:, -2]
+		t_matrix[0, :] = top_wall_temperature
+		t_matrix[-1, :] = bottom_wall_temperature
 
 		data_list[vars_list.index("U_x")] = ux_matrix
 		data_list[vars_list.index("U_y")] = uy_matrix

repitframework/plot_utils.py
@@ -139,7 +139,7 @@ def visualize_output(base_config:BaseConfig,
 	num_subplots = len(data_dict)
 	fig, ax = plt.subplots(1, num_subplots, figsize=(5*num_subplots, 5))
 	for i,(key,value) in enumerate(data_dict.items()):
-		value = ax[i].imshow(value, cmap="coolwarm")
+		value = ax[i].imshow(value)
 		fig.colorbar(value, ax=ax[i])
 		ax[i].set_title(key)
 	fig.tight_layout()
@@ -162,6 +162,7 @@ def make_animation(base_config:BaseConfig,
 					np_data_dir:Path=None, 
 					data_vars:list=None, 
 					save_name:Path=None,
+					plot_pred_gaps:bool=False,
 					set_fps:int=1)->bool:
 	'''
 	This function is used to make an animation of the output of the simulation.
@@ -172,29 +173,47 @@ def make_animation(base_config:BaseConfig,
 	save_path = save_dir / f"{save_name}.gif" if save_name else save_dir / "output.gif"
 
 	images_list = []
+	pred_time_list = [] if is_ground_truth else timestamps
+	if plot_pred_gaps:
+		min_time = min(timestamps)
+		max_time = max(timestamps)
+		interval_time = round(timestamps[1] - timestamps[0], base_config.round_to)
+		timestamps = np.round(np.arange(min_time, max_time, interval_time), base_config.round_to)
+		
 	for timestamp in timestamps:
-		images_list.append(visualize_output(base_config=base_config,
-											timestamp=timestamp,
-											np_data_dir=np_data_dir,
-											data_vars=data_vars,
-											mode="rgb_array",
-											is_ground_truth=is_ground_truth))
+		if timestamp in pred_time_list:
+			images_list.append(visualize_output(base_config=base_config,
+												timestamp=timestamp,
+												np_data_dir=np_data_dir,
+												data_vars=data_vars,
+												mode="rgb_array",
+												is_ground_truth=False))
+		else:
+			images_list.append(visualize_output(base_config=base_config,
+												timestamp=timestamp,
+												np_data_dir=np_data_dir,
+												data_vars=data_vars,
+												mode="rgb_array",
+												is_ground_truth=True))
 	imageio.mimsave(save_path, images_list, fps=set_fps, loop=0)
 	return True
 
 if __name__ == "__main__":
 
 	base_config = BaseConfig()
+	full_time_list = np.round(np.arange(10.01, 20.0, 0.01),2)
 	with open("/home/shilaj/repitframework/repitframework/ModelDump/natural_convection/prediction_metrics.json","r") as f:
 		metrics = json.load(f)
 	time_list = metrics["Running Time"]
-	print(len(time_list))
 	make_animation(base_config=base_config,
 					timestamps=time_list,
 					is_ground_truth=False,
-					set_fps=5,
-					save_name="prediction_simulation_test")
+					set_fps=50,
+					plot_pred_gaps=False,
+					save_name="prediction_simulation",
+					np_data_dir="/home/shilaj/repitframework/repitframework/Assets/natural_convection",)
 	# visualize_output(base_config=base_config,
-	# 				timestamp=10.04,
-	# 				is_ground_truth=False,
-	# 				save_name="predicted")
+	# 				timestamp=10.51,
+	# 				is_ground_truth=True,
+	# 				save_name="true",
+	# 				np_data_dir="/home/shilaj/repitframework/repitframework/Assets/natural_convection_backup",)

runner.py
@@ -67,7 +67,7 @@ class Trainer:
 		self.model.to(self.device)
 		self.optimizer = optimizer(self.model.parameters(), lr=training_config.learning_rate)
 		self.loss_fn = loss_fn
-		self.best_val_accuracy = 1
+		self.best_val_accuracy = 2.0
 
 		self.residual_threshold = training_config.residual_threshold
 		self.relative_residual_mass = float()
@@ -87,8 +87,8 @@ class Trainer:
 	def train(self, train_loader:DataLoader, 
 			  val_loader:DataLoader, 
 			  epochs) -> bool:
-		self.model.train()  # Set the model to training mode
 		for epoch in tqdm(range(epochs), desc="Epochs", leave=False):
+			self.model.train()  # Set the model to training mode
 			train_loss = 0.0
 			for x_batch, y_batch in train_loader:
 				x_batch = x_batch.to(self.device) 
@@ -162,6 +162,54 @@ class Trainer:
 		training_config.logger.info(f"Validation Loss: {val_loss:.4f}")
 		return val_loss
 	
+	def train_on_residual(self, train_loader:DataLoader,
+						  val_loader:DataLoader):
+		epoch = 0
+		while self.relative_residual_mass > 2.0:
+			self.model.train()  # Set the model to training mode
+			train_loss = 0.0
+			for x_batch, y_batch in train_loader:
+				x_batch = x_batch.to(self.device) 
+				y_batch = y_batch.to(self.device)
+
+				# Forward pass: Hard coded
+				predictions = self.model(x_batch)
+				pred_T = predictions["T"]
+				pred_ux = predictions["U_x"]
+				pred_uy = predictions["U_y"]
+
+				loss_1 = residual_mass(pred_ux, pred_uy)
+				loss_2 = residual_momentum(pred_ux, self.ux_matrix_prev, pred_uy, self.uy_matrix)
+				loss_3 = residual_heat(pred_ux, pred_uy, pred_T, self.t_matrix_prev)
+				loss = loss_1 + loss_2 + loss_3
+
+				# Backpropagation
+				self.optimizer.zero_grad()
+				loss.backward()
+				self.optimizer.step()
+
+				train_loss += loss.item()*x_batch.size(0)
+			
+			train_loss /= len(train_loader.dataset)
+			epoch += 1
+			self.training_config.log_metrics(key="Epoch", value=epoch, metrics_type="training")
+			self.training_config.log_metrics(key="Training Loss", value=train_loss, metrics_type="training")
+			training_config.logger.info(f"Epoch {epoch}, Loss: {train_loss:.4f}")
+
+			if loss < self.best_val_accuracy:
+				self.best_val_accuracy = loss
+				self.save_model(f"best_model.pth")
+
+	def _normalize(self, data:torch.Tensor, mean:np.ndarray, std:np.ndarray):
+		mean = torch.Tensor(mean).to(self.device)
+		std = torch.Tensor(std).to(self.device)
+		return (data - mean) / std
+
+	def _denormalize(self, data:torch.Tensor, mean:np.ndarray, std:np.ndarray):
+		mean = torch.Tensor(mean)
+		std = torch.Tensor(std)
+		return (data * std) + mean
+	
 	def predict(self, prediction_start_time:int|float=None, 
 				write_interval:int|float=None, 
 				data_path:Path=None):
@@ -187,20 +235,23 @@ class Trainer:
 		prediction_input = None
 		running_time = start_time # Because we saving the prediction data at prepare_input_for_prediction function. But, output is after calling this function.
 		with torch.no_grad():
+
+			# Load the mean and std from the training data: 
+			metrics_path = self.training_config.model_dir / "denorm_metrics.json"
+			with open(metrics_path, "r") as f:
+				metrics = json.load(f)
+			label_mean = np.array(metrics["label_MEAN"])
+			label_std = np.array(metrics["label_STD"])
+			input_mean = np.array(metrics["input_MEAN"])
+			input_std = np.array(metrics["input_STD"])
+
 			while (self.relative_residual_mass <= self.residual_threshold) and (running_time <= self.training_config.prediction_end_time):
-				prediction_input = self.prepare_input_for_prediction(running_time, data_path, prediction_input)
-				normalized_input,*_  = FVMNDataset.normalize(prediction_input)
+				prediction_input = self.prepare_input_for_prediction(running_time, data_path, prediction_input).to(self.device)
+				normalized_input  = self._normalize(prediction_input, input_mean, input_std)
 				predicted_output:torch.Tensor = self.model(normalized_input.to(self.device))
 				predicted_output_concat = torch.cat([output.cpu() for output in predicted_output.values()], dim=1)
-
-				# Load the mean and std from the training data: 
-				metrics_path = self.training_config.model_dir / "denorm_metrics.json"
-				with open(metrics_path, "r") as f:
-					metrics = json.load(f)
-				mean = np.array(metrics["label_MEAN"])
-				std = np.array(metrics["label_STD"])
-				denormed_output = FVMNDataset.denormalize(predicted_output_concat, mean, std)
-				prediction_input = prediction_input[:, ::5] + denormed_output
+				denormed_output = self._denormalize(predicted_output_concat, label_mean, label_std)
+				prediction_input = prediction_input[:, ::5].cpu() + denormed_output
 				running_time = round(running_time+time_step, self.training_config.round_to)
 				
 			# Because prepare_input_for_prediction function calculates the residual values.
@@ -238,7 +289,7 @@ class Trainer:
 			model_weights = model_weights["model_state_dict"]
 		self.model.load_state_dict(model_weights)
 		self.training_config.logger.info(f"Model loaded from {path}")
-		return self.model
+		return self.model.to(self.device)
 	
 	def get_ground_truth_data(self, time_step:int|float, 
 							  data_path:Path=None) -> List[np.ndarray]:
@@ -320,14 +371,14 @@ class Trainer:
 			pad_pred_data = [np.pad(pred_data[i], 1, mode="constant",constant_values=0) for i in range(len(pred_data))]
 
 			# Adding the zero padded predicted data to the zeroed internal nodes in the ground truth data.
-			final_pred_data = [np.add(t,d) for t,d in zip(ground_truth, pad_pred_data)]
+			pred_data = [np.add(t,d) for t,d in zip(ground_truth, pad_pred_data)]
 
 			# Saving the predicted data: True prediction are the ones after applying the boundary conditions.
-			self.ux_matrix = final_pred_data[self.ux_index]
-			self.uy_matrix = final_pred_data[self.uy_index]
-			self.t_matrix = final_pred_data[self.t_index] 
+			self.ux_matrix = pred_data[self.ux_index]
+			self.uy_matrix = pred_data[self.uy_index]
+			self.t_matrix = pred_data[self.t_index] 
 			
-			return final_pred_data
+			return pred_data
 
 		assert pred_data.shape[0] == self.training_config.grid_y * self.training_config.grid_x, \
 			f"Shape of the data is {pred_data.shape} but should be {(self.training_config.grid_y * self.training_config.grid_x, pred_data.shape[-1])}"
@@ -357,6 +408,7 @@ class Trainer:
 		
 		# Calculate the relative residual mass: 
 		relative_residual_mass = predicted_residual_mass / self.true_residual_mass
+		self.training_config.logger.info(f"Relative Residual Mass: {relative_residual_mass}")
 
 		self.training_config.log_metrics(key="Running Time", value=time_step)
 		self.training_config.log_metrics(key="Predicted Residual Mass", value=predicted_residual_mass)
@@ -421,7 +473,7 @@ class Trainer:
 			self.t_matrix_prev = ground_truth[self.t_index]
 			self.true_residual_mass = residual_mass(ground_truth[self.ux_index], ground_truth[self.uy_index])
 
-			if training_config.bc_type is None:
+			if not self.training_config.bc_type:
 				ground_truth = self.training_config.hard_contraint_bc(ground_truth)
 
 			temp_ = [FVMNDataset.add_feature(data) for data in ground_truth]
@@ -469,19 +521,20 @@ def main(base_config:BaseConfig,
 	framework_start_time = timeit.default_timer()
 	training_config.logger.info(f"Framework started at {framework_start_time}")
 	training_config.epochs = 1
+	first_training = True
 	while running_time < training_config.prediction_end_time:
-		print("Running time: ", running_time)
 		# Run CFD first:
 		is_solver_run = openfoam_utils.run_solver(start_time=training_start_time, 
-												  end_time=training_end_time, 
-												  save_to_numpy=True)
+												  end_time=training_end_time)
 
+		if training_end_time >= training_config.prediction_end_time: break
 		# Create dataset instance
-		dataset = dataset_type(training_config=training_config, 
+		dataset = dataset_type(training_config=training_config,
+						 		first_training=first_training, 
 							  start_time=training_start_time, 
 							  end_time=training_end_time, 
 							  time_step=training_config.write_interval)
-		train_loader, val_loader = get_dataloader(training_config, dataset)
+		train_loader, val_loader = get_dataloader(training_config, dataset, batch_size=training_config.batch_size)
 
 		# Create trainer instance
 		trainer = Trainer(training_config=training_config, model=model, optimizer=optimizer, loss_fn=loss_fn)
@@ -491,22 +544,79 @@ def main(base_config:BaseConfig,
 
 		# Before prediction, load the best model: because we are using the same instance of self.model for prediction, hence last trained parameters will be used.
 		model = trainer.load_model("best_model.pth")
+		print("\nStarting prediction from: ", round(training_end_time+training_config.write_interval,2))
 		running_time = trainer.predict(prediction_start_time=training_end_time, 
 									   write_interval=training_config.write_interval)
-
+		print(f"Prediction ended at:{running_time}\n")
 		# Convert predicted numpy to foam
-		is_numpy_to_foam = numpyToFoam(openfoam_config=openfoam_config, 
+		numpyToFoam_string = numpyToFoam(openfoam_config=openfoam_config, 
 									   latestML_time=float(running_time), 
 									   latestCFD_time=training_end_time)
 
+		openfoam_config.logger.info(f"Converted numpy to foam: {numpyToFoam_string}")
+
 		# Transfer learning
-		training_start_time = running_time
-		training_end_time = round(training_start_time + 2*training_config.write_interval,2)
-		training_config.epochs = 10
+		training_start_time = round(running_time+training_config.write_interval, 2) # Because until running time, we'd already predicted the data.
+		training_end_time = round(training_start_time + 5*training_config.write_interval,2)
+		training_config.epochs = 5
+		first_training = False
+
 
 	framework_end_time = timeit.default_timer()
 	training_config.logger.info(f"Framework ended at {framework_end_time}")
 
+def main_test(base_config:BaseConfig, 
+		openfoam_config:OpenfoamConfig, 
+		training_config:TrainingConfig,
+		network_type:torch.nn.Module=FVMNetwork,
+		dataset_type:Dataset=FVMNDataset,
+		visualize:bool=False):
+	
+	# Variables:
+	# Training
+	training_start_time = 10.52
+	training_end_time = 10.54
+	running_time = training_start_time
+	optimizer = training_config.optimizer
+	loss_fn = training_config.loss
+
+
+	training_config.prediction_end_time = 11.0
+	# Create model instance
+	model = network_type(training_config)
+	openfoam_utils = OpenfoamUtils(openfoam_config)
+
+	##################### RePIT: START #####################
+	framework_start_time = timeit.default_timer()
+	training_config.logger.info(f"Framework started at {framework_start_time}")
+	training_config.epochs = 1
+	is_solver_run = openfoam_utils.run_solver(start_time=training_start_time, 
+												  end_time=training_end_time)
+	# Create dataset instance
+	dataset = dataset_type(training_config=training_config, 
+							start_time=training_start_time, 
+							end_time=training_end_time, 
+							time_step=training_config.write_interval)
+	train_loader, val_loader = get_dataloader(training_config, dataset)
+
+	# Create trainer instance
+	trainer = Trainer(training_config=training_config, model=model, optimizer=optimizer, loss_fn=loss_fn)
+
+	# Train the model
+	trainer.train(train_loader, val_loader, training_config.epochs)
+	while running_time < training_config.prediction_end_time:
+		print("Starting prediction from: ", running_time)
+		# Before prediction, load the best model: because we are using the same instance of self.model for prediction, hence last trained parameters will be used.
+		model = trainer.load_model("best_model.pth")
+		running_time = trainer.predict(prediction_start_time=training_end_time, 
+									   write_interval=training_config.write_interval)
+
+		training_start_time = running_time
+		training_end_time = round(training_start_time + 2*training_config.write_interval,2)
+		training_config.epochs = 2
+
+	framework_end_time = timeit.default_timer()
+	training_config.logger.info(f"Framework ended at {framework_end_time}")
 
 if __name__ == "__main__":
 	openfoam_config = OpenfoamConfig()